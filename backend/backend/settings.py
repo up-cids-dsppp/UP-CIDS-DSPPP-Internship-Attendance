6f9febd309e11f1338e8486f597574f26dab352e
lemonjerome <gabrielcatadmanramos@gmail.com>
2025-04-09 10:49:33 +0800

    fix: django admin login

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,10 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',  # Add this
+    'corsheaders',
     "internship",
 ]
 
