04fc268cfdfd9d0d573e80eb4edf7a4063fcc911
lemonjerome <gabrielcatadmanramos@gmail.com>
2025-04-14 15:32:40 +0800

    feat: auto time out at 5PM

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -14,6 +14,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+# Ensure the beat scheduler is used
+app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
+
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
