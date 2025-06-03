7c3632d54641a39960742cb4c621dbe7d796a1d1
lemonjerome <gabrielcatadmanramos@gmail.com>
2025-06-03 11:18:01 +0800

    fix: admin routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('super/', admin.site.urls),
     path('internship/', include('internship.urls')),
 ]
 
